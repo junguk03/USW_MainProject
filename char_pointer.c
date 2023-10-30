309f82de35dcf9ce8fbeb952ce27fe4548c2db4b
junguk <junkuk9503@gmail.com>
2023-10-30 22:01:13 +0900

    commit

---
+++ char_pointer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    char* p = "HelloWorld";
+    printf("%s \n", p);
+    p = "Welcome to C World!";
+    printf("%s \n", p);
+    p = "Goodbye";
+    printf("%s \n", p);
+    
+    return 0;
+}
