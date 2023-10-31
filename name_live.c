0017dc1703eb7460c72cc97280a7a8be50e4b8e8
junguk <junkuk9503@gmail.com>
2023-10-31 22:03:31 +0900

    commit

---
+++ name_live.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void){
+    char name[100];
+    char address[100];
+    printf("이름을 입력하시오: ");
+    scanf("%s", name);
+    printf("현재 거주하는 주소를 입력하시오: ");
+    scanf("%s", address);
+    printf("안녕하세요, %s에 사는 %s씨.\n", address, name);
+    return 0;
+}
