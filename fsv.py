96e5cfc4d24c2730b23d0e67fb3a94ad2f16efcd
junguk <junkuk9503@gmail.com>
2023-11-10 17:09:52 +0900

    commit

---
+++ fsv.py
@@ -0,0 +1,10 @@
+def solution(s):
+    arr = []
+    for i in range(len(s)):
+        if s.find(s[i],0,i) != -1:
+            arr.insert(i,int(i - s.rfind(s[i],0,i)))
+        else:
+            arr.insert(s.find(s[i]),-1)
+    return arr
+
+print(solution("banana"))
