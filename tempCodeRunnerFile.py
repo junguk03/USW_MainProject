8c1ac6037aef7d2c20bcb7a5074e50303e076fdf
junguk <junkuk9503@gmail.com>
2023-11-12 14:15:55 +0900

    commit

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,7 @@
-            print(mode)
+def solution(s):
+    arr = list(range(0,len(s)))
+    for i in range(len(s)):
+        if s.find(s[i],i+1,len(s)) != -1:
+            arr.insert(s.find(s[i],i+1,len(s),int(s.find(s[i],i+1,len(s)) - i)))
+        arr.insert(s.find(s[i]),-1)
+    return arr
